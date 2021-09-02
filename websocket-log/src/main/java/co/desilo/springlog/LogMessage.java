eec94c7ef3feb743b4f55adba51f3588d95516b8
kungyu <hismayfly@gmail.com>
2021-09-02 10:48:04 +0900

    restlog

---
+++ websocket-log/src/main/java/co/desilo/springlog/LogMessage.java
@@ -0,0 +1,14 @@
+package co.desilo.springlog;
+
+public class LogMessage {
+
+    private String message;
+
+    public String getMessage() {
+	return this.message;
+    }
+
+    public void setMessage(String message) {
+	this.message = message;
+    }
+}
